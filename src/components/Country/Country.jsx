82c13ca5cc2b7b08439507496dc2c11168bc238f
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-06 17:36:33 +0600

    add toggle

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./Country.css";
+import { useState } from "react";
 
 const Country = ({ country }) => {
+  const [visited, setVisited] = useState(false);
+
   const handleVisited = () => {
-    console.log("visited clicked");
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    setVisited(!visited);
   };
 
   return (
-    <div className="country">
+    <div className={`country ${visited && "country-visited"}`}>
       <h3>Name: {country.name.common}</h3>
       <img src={country?.flags?.png} alt="" />
       <p>Independent: {country.independent ? "Free" : "Not Free"} </p>
       <p>Population: {country.population}</p>
-      <button onClick={handleVisited}>Visited</button>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
